14534d710dd5b18febe204b9b8d0e54d2d8059d5
Tynahan <134694901+ThePott@users.noreply.github.com>
2025-11-27 18:34:56 +0900

    feat: add css variables

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,11 @@
 import { FullScreen } from "@/packages/layouts"
 
 const LandingPage = () => {
-    return <FullScreen isCentered>ARBOR</FullScreen>
+    return (
+        <FullScreen isCentered className="text-fg-vivid gap-my-xl">
+            <div className="bg-bg-2 shadow-my-xl">ARBOR</div>
+        </FullScreen>
+    )
 }
 
 export default LandingPage
