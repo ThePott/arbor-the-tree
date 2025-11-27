adc1022ae48a0b14196183e85c94a966cec5356e
Tynahan <134694901+ThePott@users.noreply.github.com>
2025-11-27 19:59:45 +0900

    feat(common): add flex one container

---
+++ src/packages/layouts/_FlexOneContainer.tsx
@@ -0,0 +1,37 @@
+import type { DivProps } from "@/shared/interfaces"
+import { cva } from "class-variance-authority"
+import clsx from "clsx"
+
+const flexOneContainerVariants = cva("flex-1", {
+    variants: {
+        isYScrollable: {
+            true: "overflow-y-scroll",
+            false: "overflow-y-hidden",
+        },
+        isXScrollable: {
+            true: "overflow-x-scroll",
+            false: "overflow-x-hidden",
+        },
+    },
+})
+
+interface WithFlexOneContainer {
+    isYScrollable?: boolean
+    isXScrollable?: boolean
+}
+
+const FlexOneContainer = ({
+    isYScrollable = false,
+    isXScrollable = false,
+    ...props
+}: DivProps & WithFlexOneContainer) => {
+    const { className, children, ...rest } = props
+
+    return (
+        <div {...rest} className={clsx(flexOneContainerVariants({ isYScrollable, isXScrollable }), className)}>
+            {children}
+        </div>
+    )
+}
+
+export default FlexOneContainer
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,23 +1,14 @@
-import { FullScreen, GridContainer } from "@/packages/layouts"
+import { FullScreen, Vstack } from "@/packages/layouts"
+import FlexOneContainer from "@/packages/layouts/_FlexOneContainer"
 
 const LandingPage = () => {
     return (
         <FullScreen isCentered>
             <div className="bg-bg-2 shadow-my-xl w-[800px]">ARBOR</div>
-            <GridContainer isAutoFill className="w-[800px]">
+            <Vstack className="h-[800px]">
                 <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-                <p>asdf</p>
-            </GridContainer>
+                <FlexOneContainer className="bg-amber-600"> asdf</FlexOneContainer>
+            </Vstack>
         </FullScreen>
     )
 }
