660458ffa073f29b876fab74184b327d469ca663
Tynahan <134694901+ThePott@users.noreply.github.com>
2025-12-02 15:55:45 +0900

    chore(temp): temp fix to push

--- src/pages/layout/Header/LoginButton/kakaoLoginApi.ts
+++ src/pages/layout/Header/LoginButton/kakaoLoginApi.ts
@@ -5,7 +5,10 @@ const kakaoLogin = async (code: string) => {
     const { access_token } = accessTokenResponse.data
     const meResponse = await headlessInstance.post("/auth/kakao/me", { access_token })
     const kakaoMe = meResponse.data
-    debugger
+    // TODO: 이걸 스토어에 등록해야 함... <- access, refresh, me 어떻게 클라이언트에서 관리할지 고민해야
+
+    // NOTE: THIS IS TEMPORARY
+    return kakaoMe
 }
 
 export default kakaoLogin
