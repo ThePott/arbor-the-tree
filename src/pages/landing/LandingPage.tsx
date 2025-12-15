10b0f1e01b5e2f8ef24596c1e1c159dbdbab85d9
Tynahan <nusilite@gmail.com>
2025-12-15 13:22:49 +0900

    chore(debug): remove debug components

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,15 +1,11 @@
-import Button from "@/packages/components/Button"
 import { FullScreen, Vstack } from "@/packages/components/layouts"
 import DropAnimation from "@/packages/components/motions/DropAnimation"
 import RoundBox from "@/packages/components/RoundBox"
-import { useState } from "react"
 
 const LandingPage = () => {
-    const [isOn, setIsOn] = useState(false)
     return (
         <FullScreen isCentered>
-            <Button onClick={() => setIsOn(!isOn)}>Toggle</Button>
-            <DropAnimation isOn={isOn} intensity="lg">
+            <DropAnimation isOn={true} intensity="lg">
                 <RoundBox color="bg2" className="px-24 py-[48px]" isShadowed radius="lg">
                     <Vstack className="gap-my-lg items-center">
                         <h1 className="mr-[-48px] text-[96px] font-extralight tracking-[48px]">ARBOR</h1>
