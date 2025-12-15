fe823ba7e721c347f92d377c57dfccf5c5f56bb5
Tynahan <nusilite@gmail.com>
2025-12-15 13:07:57 +0900

    feat(animation): add drop animation wrapper

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,16 +1,23 @@
+import Button from "@/packages/components/Button"
 import { FullScreen, Vstack } from "@/packages/components/layouts"
+import DropAnimation from "@/packages/components/motions/DropAnimation"
 import RoundBox from "@/packages/components/RoundBox"
+import { useState } from "react"
 
 const LandingPage = () => {
+    const [isOn, setIsOn] = useState(false)
     return (
         <FullScreen isCentered>
-            <RoundBox color="bg2" className="blur-drop px-24 py-[48px]" isShadowed radius="lg">
-                <Vstack className="gap-my-lg items-center">
-                    <h1 className="mr-[-48px] text-[96px] font-extralight tracking-[48px]">ARBOR</h1>
-                    <h2 className="text-my-xl">arbor: [Latin] tree</h2>
-                    <p>이제 숲으로 성장할 차례입니다</p>
-                </Vstack>
-            </RoundBox>
+            <Button onClick={() => setIsOn(!isOn)}>Toggle</Button>
+            <DropAnimation isOn={isOn} intensity="lg">
+                <RoundBox color="bg2" className="px-24 py-[48px]" isShadowed radius="lg">
+                    <Vstack className="gap-my-lg items-center">
+                        <h1 className="mr-[-48px] text-[96px] font-extralight tracking-[48px]">ARBOR</h1>
+                        <h2 className="text-my-xl">arbor: [Latin] tree</h2>
+                        <p>이제 숲으로 성장할 차례입니다</p>
+                    </Vstack>
+                </RoundBox>
+            </DropAnimation>
         </FullScreen>
     )
 }
--- src/testRoutes/_AnimationEventTestPage.tsx
+++ src/testRoutes/_AnimationEventTestPage.tsx
@@ -1,79 +1,32 @@
 import Button from "@/packages/components/Button"
-import { Vstack } from "@/packages/components/layouts"
 import Container from "@/packages/components/layouts/_Container"
 import RoundBox from "@/packages/components/RoundBox"
-import { cva } from "class-variance-authority"
-import clsx from "clsx"
-import { Activity, useEffect, useState, type ReactNode } from "react"
-import "./_AnimationTestPage.css"
-
-const animationWrapperVariants = cva("", {
-    variants: {
-        isOn: {
-            true: "drop-in-md",
-            false: "blur-drop",
-        },
-    },
-})
-
-interface AnimationWrapperProps {
-    isOn: boolean
-    children: ReactNode
-}
-
-/**
- * MUST USE isOn in key
- */
-const AnimationWrapper = ({ isOn, children }: AnimationWrapperProps) => {
-    const [isVisible, setIsVisible] = useState<boolean | null>(null)
-
-    useEffect(() => {
-        if (isVisible !== null) {
-            return
-        }
-
-        setIsVisible(isOn)
-    }, [isOn])
-
-    const handleAnimationEnd = () => {
-        if (isOn === isVisible) {
-            setIsVisible(!isOn)
-        }
-
-        return
-    }
-
-    if (!isVisible) {
-        return null
-    }
-
-    return (
-        <div onAnimationEnd={handleAnimationEnd} className={clsx(animationWrapperVariants({ isOn }))}>
-            {children}
-        </div>
-    )
-}
-
-const DummyBox = () => {
-    return (
-        <RoundBox padding="xl" isBordered color="bg2">
-            this is something
-        </RoundBox>
-    )
-}
+import { AnimatePresence, motion } from "motion/react"
+import { useState } from "react"
 
 const AnimationEventTestPage = () => {
-    const [isOn, setIsOn] = useState(false)
+    const [isVisible, setIsVisible] = useState(true)
 
     return (
-        <Container width="md" isPadded>
-            <RoundBox padding="xl" className="drop-in-md">
-                <Vstack>
-                    <Button onClick={() => setIsOn(!isOn)}>{JSON.stringify(isOn)}</Button>
-                    <AnimationWrapper isOn={isOn}>
-                        <DummyBox />
-                    </AnimationWrapper>
-                </Vstack>
+        <Container>
+            <RoundBox isShadowed>
+                <AnimatePresence initial={false}>
+                    <motion.div>
+                        <Button onClick={() => setIsVisible(!isVisible)}>{isVisible ? "Hide" : "Show"}</Button>
+                    </motion.div>
+                    {isVisible ? (
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            exit={{ opacity: 0, scale: 0 }}
+                            key="box"
+                        >
+                            <RoundBox color="bg2" isShadowed padding="xl">
+                                여기에다가 아무거나 적자
+                            </RoundBox>
+                        </motion.div>
+                    ) : null}
+                </AnimatePresence>
             </RoundBox>
         </Container>
     )
