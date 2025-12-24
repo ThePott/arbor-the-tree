641e3792c63cfc8ddfd4255c6c700dcfe2079596
Tynahan <nusilite@gmail.com>
2025-12-24 19:16:24 +0900

    feat(book write): add tab

--- src/features/book/BookListTable/write/BookWriteContent.tsx
+++ src/features/book/BookListTable/write/BookWriteContent.tsx
@@ -4,6 +4,7 @@ import RoundBox from "@/packages/components/RoundBox"
 import useBookWriteStore from "./_bookWriteStore"
 import Title from "@/packages/components/Title/Title"
 import Textarea from "@/packages/components/Textarea/Textarea"
+import TabBar from "@/packages/components/TabBar/TabBar"
 
 const BookWriteContent = () => {
     const title = useBookWriteStore((state) => state.title)
@@ -24,6 +25,14 @@ const BookWriteContent = () => {
                         <Title as="h2" isMuted>
                             단원 정보 기입
                         </Title>
+                        <TabBar
+                            tabArray={[
+                                { value: "topic", label: "중단원" },
+                                { value: "step", label: "소단원" },
+                            ]}
+                            variant="underline"
+                            onSelect={() => {}}
+                        />
                         <Textarea className="flex-1" />
                     </Vstack>
                 </RoundBox>
