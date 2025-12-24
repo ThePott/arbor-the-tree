bb0b1e048a012719b77f9aed32e7e6ec466657c2
Tynahan <134694901+ThePott@users.noreply.github.com>
2025-12-24 14:21:13 +0900

    feat(table): show table body

--- src/testRoutes/_TableDummyColumns.tsx
+++ src/testRoutes/_TableDummyColumns.tsx
@@ -4,9 +4,9 @@ import type { Book } from "./_TableDummyData"
 const columnHelper = createColumnHelper<Book>()
 
 export const tableDummyColumns = [
-    columnHelper.accessor("title", { header: "문제집 제목" }),
-    columnHelper.accessor("publishedYear", { header: "출간년도" }),
-    columnHelper.accessor("modifiedAt", { header: "수정일" }),
+    columnHelper.accessor("title", { header: "문제집 제목", cell: (info) => info.getValue() }),
+    columnHelper.accessor("publishedYear", { header: "출간년도", cell: (info) => info.getValue() }),
+    columnHelper.accessor("modifiedAt", { header: "수정일", cell: (info) => info.getValue() }),
     // columnHelper.display({
     //     id: "delete",
     //     cell: (props) => <p>{JSON.stringify(props)}</p>,
--- src/testRoutes/_TableTestPage.tsx
+++ src/testRoutes/_TableTestPage.tsx
@@ -1,11 +1,19 @@
 import { Container } from "@/packages/components/layouts"
 import RoundBox from "@/packages/components/RoundBox"
-import { getCoreRowModel, useReactTable, type Header } from "@tanstack/react-table"
+import { flexRender, getCoreRowModel, useReactTable, type Header, type Row } from "@tanstack/react-table"
 import { tableDummyColumns } from "./_TableDummyColumns"
 import { tableDummyData, type Book } from "./_TableDummyData"
 
 const HeaderCell = ({ header }: { header: Header<Book, unknown> }) => {
     return <>{header.column.columnDef.header}</>
+}
+
+const RowComp = ({ row }: { row: Row<Book> }) => {
+    return <>this is row</>
+}
+
+const BodyCell = () => {
+    return <>this is body cell</>
 }
 
 const TableTestPage = () => {
@@ -19,11 +27,28 @@ const TableTestPage = () => {
         <Container width="lg" isPadded>
             <RoundBox isBordered padding="xl">
                 <table>
-                    <tr>
-                        {table.getFlatHeaders().map((header) => (
-                            <HeaderCell header={header} />
+                    <thead>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <tr>
+                                {headerGroup.headers.map((header) => (
+                                    <th>
+                                        {header.isPlaceholder
+                                            ? "this is header placeholder"
+                                            : flexRender(header.column.columnDef.header, header.getContext())}
+                                    </th>
+                                ))}
+                            </tr>
                         ))}
-                    </tr>
+                    </thead>
+                    <tbody>
+                        {table.getRowModel().rows.map((row) => (
+                            <tr>
+                                {row.getVisibleCells().map((cell) => (
+                                    <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
                 </table>
             </RoundBox>
         </Container>
