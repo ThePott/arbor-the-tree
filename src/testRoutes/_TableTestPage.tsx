e18ccfd2c666ffc781dbb997b27d436c2e54ebdb
Tynahan <134694901+ThePott@users.noreply.github.com>
2025-12-24 14:33:27 +0900

    style(table): add basic style of table

--- src/testRoutes/_TableTestPage.tsx
+++ src/testRoutes/_TableTestPage.tsx
@@ -1,20 +1,8 @@
 import { Container } from "@/packages/components/layouts"
 import RoundBox from "@/packages/components/RoundBox"
-import { flexRender, getCoreRowModel, useReactTable, type Header, type Row } from "@tanstack/react-table"
+import { flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table"
 import { tableDummyColumns } from "./_TableDummyColumns"
-import { tableDummyData, type Book } from "./_TableDummyData"
-
-const HeaderCell = ({ header }: { header: Header<Book, unknown> }) => {
-    return <>{header.column.columnDef.header}</>
-}
-
-const RowComp = ({ row }: { row: Row<Book> }) => {
-    return <>this is row</>
-}
-
-const BodyCell = () => {
-    return <>this is body cell</>
-}
+import { tableDummyData } from "./_TableDummyData"
 
 const TableTestPage = () => {
     const table = useReactTable({
@@ -31,7 +19,7 @@ const TableTestPage = () => {
                         {table.getHeaderGroups().map((headerGroup) => (
                             <tr>
                                 {headerGroup.headers.map((header) => (
-                                    <th>
+                                    <th className="border-border-dim hover:outline-border-muted z-10 border px-3 py-2 hover:outline">
                                         {header.isPlaceholder
                                             ? "this is header placeholder"
                                             : flexRender(header.column.columnDef.header, header.getContext())}
@@ -44,7 +32,12 @@ const TableTestPage = () => {
                         {table.getRowModel().rows.map((row) => (
                             <tr>
                                 {row.getVisibleCells().map((cell) => (
-                                    <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+                                    <td
+                                        key={cell.id}
+                                        className="border-border-dim hover:outline-border-muted z-10 border px-3 py-2 hover:outline"
+                                    >
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </td>
                                 ))}
                             </tr>
                         ))}
