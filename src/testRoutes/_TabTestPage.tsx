7747ff1752472842653ff14a1c655a572c0870fc
Tynahan <nusilite@gmail.com>
2025-12-24 11:17:38 +0900

    fix(lint): fix lint error

--- src/testRoutes/_TabTestPage.tsx
+++ src/testRoutes/_TabTestPage.tsx
@@ -1,19 +1,15 @@
 import { Container } from "@/packages/components/layouts"
 import RoundBox from "@/packages/components/RoundBox"
-import TabBar from "@/packages/components/TabBar/TabBar"
 import { useState } from "react"
 
-const tabArray = ["something", "st", "something very long"]
-
 const TabTestPage = () => {
-    const [selectedLabel, setSelectedLabel] = useState("something")
+    const [selectedLabel, _setSelectedLabel] = useState("something")
 
     return (
         <Container width="lg" isPadded>
             <RoundBox padding="xl" isBordered>
                 <div>Tab Test Page</div>
                 <div>{selectedLabel}</div>
-                <TabBar variant="underline" tabArray={tabArray} onSelect={(tab) => setSelectedLabel(tab)} />
             </RoundBox>
         </Container>
     )
