01de2ba765fa84965a585275083bb58d28c20355
Tynahan <nusilite@gmail.com>
2025-12-25 20:21:53 +0900

    feat(book write): add empty table

--- src/features/book/write/BWTable/BWTable.tsx
+++ src/features/book/write/BWTable/BWTable.tsx
@@ -1,15 +1,75 @@
 import { FlexOneContainer } from "@/packages/components/layouts"
 import { Vstack } from "@/packages/components/layouts/_Vstack"
 import Title from "@/packages/components/Title/Title"
+import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table"
+import clsx from "clsx"
+
+interface BookDetail {
+    topic: string
+    step: string
+    question_name: string
+    question_page: number
+    solution_page: number
+    session: number
+}
+
+const data: BookDetail[] = Array(1000).fill({}) as BookDetail[]
+
+const columnHelper = createColumnHelper<BookDetail>()
+
+const columns = [
+    columnHelper.accessor("topic", { header: "중단원", cell: (info) => info.getValue() }),
+    columnHelper.accessor("step", { header: "소단원", cell: (info) => info.getValue() }),
+    columnHelper.accessor("question_name", { header: "문제 번호", cell: (info) => info.getValue() }),
+    columnHelper.accessor("question_page", { header: "문제 쪽 번호", cell: (info) => info.getValue() }),
+    columnHelper.accessor("solution_page", { header: "정답 쪽 번호", cell: (info) => info.getValue() }),
+    columnHelper.accessor("session", { header: "묶음 번호", cell: (info) => info.getValue() }),
+]
 
 const BWTable = () => {
+    const table = useReactTable({ data, columns, getCoreRowModel: getCoreRowModel() })
+
     return (
         <Vstack className="h-full grow">
             <Title as="h2" isMuted>
                 문제 정보 기입
             </Title>
             <FlexOneContainer isYScrollable>
-                <div className="bg-bg-3 h-[1000px] w-full rounded-4xl" />
+                <table className="w-full">
+                    <thead>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <tr key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => (
+                                    <th
+                                        key={header.id}
+                                        className="border-border-dim hover:outline-border-muted z-10 border px-3 py-2 hover:outline"
+                                    >
+                                        {header.isPlaceholder
+                                            ? "this is header placeholder"
+                                            : flexRender(header.column.columnDef.header, header.getContext())}
+                                    </th>
+                                ))}
+                            </tr>
+                        ))}
+                    </thead>
+                    <tbody>
+                        {table.getRowModel().rows.map((row) => (
+                            <tr key={row.id}>
+                                {row.getVisibleCells().map((cell) => (
+                                    <td
+                                        key={cell.id}
+                                        className={clsx(
+                                            "border-border-dim hover:outline-border-muted z-10 border px-3 py-2 hover:outline",
+                                            cell.column.id !== "title" && "text-center"
+                                        )}
+                                    >
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </FlexOneContainer>
         </Vstack>
     )
