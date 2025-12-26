30142d1ae9bddba6ef443de8afb4f17ae58b6408
Tynahan <nusilite@gmail.com>
2025-12-26 22:03:32 +0900

    style(book write): mute overlayed text

--- src/features/book/write/BWTable/_BWInputCell.tsx
+++ src/features/book/write/BWTable/_BWInputCell.tsx
@@ -5,13 +5,14 @@ import useBookWriteStore from "../_bookWriteStore"
 
 const BWInputCell = ({ getValue, row: { index }, column: { id } }: CellContext<BookDetail, string>) => {
     const updateTableData = useBookWriteStore((state) => state.updateTableData)
-    const handleUpdate = (event: React.FocusEvent<HTMLInputElement, Element>) =>
+    const handleUpdate = (event: React.FocusEvent<HTMLInputElement, Element>) => {
         updateTableData(index, id as keyof BookDetail, event.target.value)
+    }
 
     return (
         <div className="relative">
             <Input colorChangeIn="fill" variant="ghost" defaultValue={getValue()} onBlur={handleUpdate} />{" "}
-            <p className="text-fg-muted absolute top-1/2 left-6 -translate-y-1/2">asdfasdf</p>
+            <p className="text-fg-muted pointer-events-none absolute top-1/2 left-6 -translate-y-1/2">asdfasdf</p>
         </div>
     )
 }
