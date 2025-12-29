9049075e2ff5629086d0c886bc120642a0990123
Tynahan <nusilite@gmail.com>
2025-12-29 15:03:52 +0900

    fix(book write): display overlaying step

--- src/features/book/write/_bookWriteStoreOperations.ts
+++ src/features/book/write/_bookWriteStoreOperations.ts
@@ -9,28 +9,13 @@ export const makeNewOverlayingValue = (
 ): string => {
     const rowRightAbove = state.overlayingRowArray[rowIndex - 1]
 
-    if (columnKey === "topic") {
+    if (columnKey === "topic" || columnKey === "step") {
         if (value !== "/") return value
 
-        const topicArray = state.topicArray
+        const lineArray = columnKey === "topic" ? state.topicArray : state.stepArray
         const valueRightAbove = rowRightAbove ? rowRightAbove[columnKey] : null
-        const indexOfValueRightAbove = topicArray.findIndex((topic) => topic === valueRightAbove)
-        const newValue = topicArray[indexOfValueRightAbove + 1]
-        if (!newValue) throw new Error("---- 중단원, 소단원 정보를 더 채워야 합니다")
-        return newValue
-    }
-
-    if (columnKey === "step") {
-        if (value !== "/") throw new Error("---- 중단원, 소단원에는 `/`만 적을 수 있습니다")
-
-        const stepArray = state.stepArray
-        const valueRightAbove = rowRightAbove
-            ? rowRightAbove[columnKey]
-            : (stepArray[0] ?? "중단원, 소단원 정보를 먼저 기입해주세요")
-        const indexOfValueRightAbove = stepArray.findIndex((topic) => topic === valueRightAbove)
-        if (indexOfValueRightAbove === -1) throw new Error("---- 중단원, 소단원 정보를 더 채워야 합니다")
-        const newValue = stepArray[indexOfValueRightAbove + 1]
-        if (!newValue) throw new Error("---- 중단원, 소단원 정보를 더 채워야 합니다")
+        const indexOfValueRightAbove = lineArray.findIndex((line) => line === valueRightAbove)
+        const newValue = lineArray[indexOfValueRightAbove + 1] ?? "단원 정보를 더 채워야 합니다"
         return newValue
     }
 
