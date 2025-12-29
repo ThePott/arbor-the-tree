446aa63cd4e913cec6866f14620c5f416aa32d55
Tynahan <nusilite@gmail.com>
2025-12-29 21:06:40 +0900

    feat(virtual scroll): setup test page

--- src/testRoutes/_TableTestPage.tsx
+++ src/testRoutes/_TableTestPage.tsx
@@ -1,7 +1,19 @@
+import Input from "@/packages/components/Input/Input"
 import RoundBox from "@/packages/components/RoundBox"
+import { useState } from "react"
 
 const TableTestPage = () => {
-    return <RoundBox padding="xl" isBordered></RoundBox>
+    const [value, setValue] = useState("")
+
+    return (
+        <RoundBox padding="xl" isBordered>
+            {Array(2000)
+                .fill(null)
+                .map((_, index) => (
+                    <Input key={index} value={value} onChange={(event) => setValue(event.target.value)} />
+                ))}
+        </RoundBox>
+    )
 }
 
 export default TableTestPage
