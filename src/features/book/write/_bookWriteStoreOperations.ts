0f99e46b5405a27a131c8df1f0fb6282cfa55531
Tynahan <nusilite@gmail.com>
2025-12-29 14:48:35 +0900

    feat(book write): add topic, step overlaying logic

--- src/features/book/write/_bookWriteStoreOperations.ts
+++ src/features/book/write/_bookWriteStoreOperations.ts
@@ -7,20 +7,38 @@ export const makeNewOverlayingValue = (
     value: string,
     state: BookWriteStoreState
 ): string => {
+    const rowRightAbove = state.overlayingRowArray[rowIndex - 1]
+
     if (columnKey === "topic") {
-        if (value !== "/") {
-            throw new Error("---- 중단원, 소단원에는 `/`만 적을 수 있습니다")
-        }
-        return "placeholder"
-    }
-    if (columnKey === "step") {
-        if (value !== "/") {
-            throw new Error("---- 중단원, 소단원에는 `/`만 적을 수 있습니다")
-        }
-        return "placeholder"
+        if (value !== "/") throw new Error("---- 중단원, 소단원에는 `/`만 적을 수 있습니다")
+
+        const topicArray = state.topicArray
+        const valueRightAbove = rowRightAbove
+            ? rowRightAbove[columnKey]
+            : (topicArray[0] ?? "중단원, 소단원 정보를 먼저 기입해주세요")
+        const indexOfValueRightAbove = topicArray.findIndex((topic) => topic === valueRightAbove)
+        if (indexOfValueRightAbove === -1) throw new Error("---- 중단원, 소단원 정보를 더 채워야 합니다")
+        const newValue = topicArray[indexOfValueRightAbove + 1]
+        if (!newValue) throw new Error("---- 중단원, 소단원 정보를 더 채워야 합니다")
+        return newValue
     }
 
-    const rowRightAbove = state.overlayingRowArray[rowIndex - 1]
+    debugger
+
+    if (columnKey === "step") {
+        if (value !== "/") throw new Error("---- 중단원, 소단원에는 `/`만 적을 수 있습니다")
+
+        const stepArray = state.stepArray
+        const valueRightAbove = rowRightAbove
+            ? rowRightAbove[columnKey]
+            : (stepArray[0] ?? "중단원, 소단원 정보를 먼저 기입해주세요")
+        const indexOfValueRightAbove = stepArray.findIndex((topic) => topic === valueRightAbove)
+        if (indexOfValueRightAbove === -1) throw new Error("---- 중단원, 소단원 정보를 더 채워야 합니다")
+        const newValue = stepArray[indexOfValueRightAbove + 1]
+        if (!newValue) throw new Error("---- 중단원, 소단원 정보를 더 채워야 합니다")
+        return newValue
+    }
+
     const valueRightAbove = rowRightAbove ? rowRightAbove[columnKey] : "1"
     const newValue = value === "/" ? String(Number(valueRightAbove) + 1) : value
 
