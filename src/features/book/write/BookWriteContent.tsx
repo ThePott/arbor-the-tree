a8b2074346ef880c1dbb90e480a85d1974d326f4
Tynahan <nusilite@gmail.com>
2025-12-30 12:32:26 +0900

    refactor(book write): separate submit handler

--- src/features/book/write/BookWriteContent.tsx
+++ src/features/book/write/BookWriteContent.tsx
@@ -4,39 +4,12 @@ import useBookWriteStore from "./_bookWriteStore"
 import BWTopicStepSection from "./BWTopicStepSection/BWTopicStepSection"
 import BWSubBookSection from "./BWSubBookSection/BWSubBookSection"
 import BWTable from "./BWTable/BWTable"
+import useBookWriteEventHandler from "./_bookWriteEventHandlers"
 
 const BookWriteContent = () => {
     const title = useBookWriteStore((state) => state.title)
     const setTitle = useBookWriteStore((state) => state.setTitle)
-    // const rowArray = useBookWriteStore((state) => state.rowArray)
-    // const overlayingRowArray = useBookWriteStore((state) => state.overlayingRowArray)
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        // const data: BookDetail[] = rowArray
-        //     .filter((row) => row.question_name)
-        //     .map((row, rowIndex) => ({
-        //         topic: row.topic && row.topic !== "/" ? row.topic : overlayingRowArray[rowIndex].topic,
-        //         step: row.step && row.step !== "/" ? row.step : overlayingRowArray[rowIndex].step,
-        //         question_name: row.question_name,
-        //         question_page:
-        //             row.question_page && row.question_page !== "/"
-        //                 ? row.question_page
-        //                 : overlayingRowArray[rowIndex].question_page,
-        //         solution_page:
-        //             row.solution_page && row.solution_page !== "/"
-        //                 ? row.solution_page
-        //                 : overlayingRowArray[rowIndex].solution_page,
-        //         session: row.session && row.session !== "/" ? row.session : overlayingRowArray[rowIndex].session,
-        //         sub_question_name: row.sub_question_name,
-        //     }))
-
-        //NOTE: 문제 번호에
-        //NOTE: uncomment to debug
-        // const body = { title, data }
-        // console.log({ body })
-        // debugger
-    }
+    const { handleSubmit } = useBookWriteEventHandler()
 
     return (
         <form className="h-full w-full" onSubmit={handleSubmit}>
