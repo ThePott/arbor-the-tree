6e1135c0eacbb8ffd5dbafc14e4bdc302d533e3f
Tynahan <nusilite@gmail.com>
2026-01-05 15:54:32 +0900

    fix(book write): revalidate error when value has changed

--- src/features/book/write/_bookWriteStore.ts
+++ src/features/book/write/_bookWriteStore.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand"
 import type { BookWriteStoreState } from "./_bookWriteStoreState"
 import { BW_DEFAULT_ROW_COUNT, BW_TOPIC_STEP_TAB_ARRAY } from "./_bookWriteConstants"
-import type { BookWriteRow } from "./_bookWriteInterfaces"
+import { BOOK_DETAIL_KEY_ARRAY, type BookWriteRow } from "./_bookWriteInterfaces"
 import { splitByLineBreakThenTrim } from "@/shared/utils/stringManipulation"
 import { recalculateColumn } from "./_bookWriteStoreOperations"
 import { createJSONStorage, persist } from "zustand/middleware"
@@ -58,12 +58,41 @@ const useBookWriteStore = create<BookWriteStoreState>()(
                 ),
             updateRowArray: (rowIndex, columnKey, value) => {
                 const rowArray = [...get().rowArray]
-                if (!value && !rowArray[rowIndex][columnKey].value) return
+                const row = rowArray[rowIndex]
+                const cell = row[columnKey]
+                if (!value && !row[columnKey].value) return
 
-                rowArray[rowIndex][columnKey].value = value
+                cell.value = value
+                cell.isError = false
 
-                if (value && !rowArray[rowIndex].question_name.value) {
-                    rowArray[rowIndex].question_name.isError = true
+                // NOTE: 문제를 지웠는데...
+                if (!value && columnKey === "question_name") {
+                    const lastQuestionIndex = rowArray.reduce((acc, row, index) => {
+                        if (row.question_name.value) return index
+                        return acc
+                    }, 0)
+
+                    // NOTE: 문제를 지웠는데 중간을 지운 거라면 오류
+                    if (rowIndex < lastQuestionIndex) {
+                        cell.isError = true
+                    } else {
+                        const { question_name: _question_name, ...rest } = row
+                        const isClearingOverlay = Object.entries(rest).every(([_key, column]) => !column.value)
+                        if (isClearingOverlay) {
+                            // NOTE: 문제를 지웠는데 같은 행 다른 열에 값이 없으면 모든 오버레이 초기화
+                            Object.entries(row).forEach(([_key, column]) => {
+                                column.overlaying = ""
+                            })
+                        } else {
+                            // NOTE: 다른 값이 있으면 오류 띄우기
+                            cell.isError = true
+                        }
+                    }
+                }
+
+                // NOTE: 문제 말고 다른 걸 입력했는데 문제가 없으면 -> 오류
+                if (value && !row.question_name.value) {
+                    row.question_name.isError = true
                 }
 
                 recalculateColumn({ rowArray, columnKey })
