ac6331e42d36358f61db5ca7cef12a2fad7ef5a3
Tynahan <nusilite@gmail.com>
2026-01-15 15:53:09 +0900

    feat(manage): extend type for other classrooms

--- src/featuresPerRoute/manage.student/types/index.ts
+++ src/featuresPerRoute/manage.student/types/index.ts
@@ -1,4 +1,6 @@
 import type { AppUser, Classroom, ClassroomStudent, School, Student } from "@/shared/interfaces"
 
 export type ExtendedStudent = Student & { school: School } & { users: AppUser }
-export type ExtendedClassroom = Classroom & { classroomStudents: (ClassroomStudent & { student: ExtendedStudent })[] }
+export type ExtendedClassroom = Classroom & {
+    classroomStudents: (ClassroomStudent & { student: ExtendedStudent & { classroomStudents: ClassroomStudent[] } })[]
+}
