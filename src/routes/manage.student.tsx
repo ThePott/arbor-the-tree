d35e0f9e85d29f7cd2176b5e69ba4d25292243c9
Tynahan <nusilite@gmail.com>
2026-01-19 16:44:34 +0900

    feat(manage): add search params

--- src/routes/manage.student.tsx
+++ src/routes/manage.student.tsx
@@ -2,9 +2,15 @@ import manageStudentLoaderFn from "@/featuresPerRoute/manage.student/loader"
 import ManageStudentPage from "@/featuresPerRoute/manage.student/page"
 import ManageStudentPending from "@/featuresPerRoute/manage.student/pending"
 import { createFileRoute } from "@tanstack/react-router"
+import z from "zod/v3"
+
+const manageStudentSearchSchema = z.object({
+    by: z.enum(["classroom", "student"]).optional(),
+})
 
 export const Route = createFileRoute("/manage/student")({
     component: ManageStudentPage,
     pendingComponent: ManageStudentPending,
     loader: ({ context: { queryClient } }) => manageStudentLoaderFn({ queryClient }),
+    validateSearch: manageStudentSearchSchema,
 })
