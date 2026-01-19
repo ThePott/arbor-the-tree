d85f9b68175e3f9138acb0f53ab079d2d9f1e831
Tynahan <nusilite@gmail.com>
2026-01-20 03:08:20 +0900

    feat(sidebar): add router button

--- src/featuresPerRoute/root/Sidebar/index.tsx
+++ src/featuresPerRoute/root/Sidebar/index.tsx
@@ -1,7 +1,9 @@
 import Button from "@/packages/components/Button/Button"
-import { Hstack } from "@/packages/components/layouts"
+import Labeled from "@/packages/components/Labeled/Labeled"
+import { Hstack, Vstack } from "@/packages/components/layouts"
 import RoundBox from "@/packages/components/RoundBox"
 import useGlobalStore from "@/shared/store/globalStore"
+import { useNavigate } from "@tanstack/react-router"
 import { X } from "lucide-react"
 import { AnimatePresence, motion } from "motion/react"
 import { useLayoutEffect } from "react"
@@ -10,6 +12,8 @@ const Sidebar = () => {
     const isSidebarOn = useGlobalStore((state) => state.isSidebarOn)
     const setIsSidebarOn = useGlobalStore((state) => state.setIsSidebarOn)
     const setIsBodyScrollable = useGlobalStore((state) => state.setIsBodyScrollable)
+
+    const navigate = useNavigate()
 
     useLayoutEffect(() => {
         setIsBodyScrollable(!isSidebarOn)
@@ -32,17 +36,71 @@ const Sidebar = () => {
                         className="h-full"
                     >
                         <RoundBox
-                            padding="md"
+                            padding="lg"
+                            radius="lg"
                             color="bg2"
-                            className="absolute top-0 left-0 h-full rounded-l-none w-50"
+                            className="absolute top-0 left-0 h-full rounded-l-none w-[300px]"
                             onClick={(event) => event.stopPropagation()}
                         >
-                            <Hstack className="justify-end">
-                                <Button color="bg2" isBorderedOnHover onClick={() => setIsSidebarOn(false)}>
-                                    <X size={16} />
-                                </Button>
-                            </Hstack>
-                            <p>this is sidebar</p>
+                            <Vstack gap="md">
+                                <Hstack className="justify-end">
+                                    <Button color="bg2" isBorderedOnHover onClick={() => setIsSidebarOn(false)}>
+                                        <X size={16} />
+                                    </Button>
+                                </Hstack>
+
+                                <Labeled>
+                                    <Labeled.Header>진도표</Labeled.Header>
+                                    <RoundBox isBordered padding="md">
+                                        <Vstack gap="xs">
+                                            <Button
+                                                isOnLeft
+                                                onClick={() => {
+                                                    setIsSidebarOn(false)
+                                                    navigate({ to: "/progress" })
+                                                }}
+                                            >
+                                                전체
+                                            </Button>
+                                            <Button
+                                                isOnLeft
+                                                onClick={() => {
+                                                    setIsSidebarOn(false)
+                                                    navigate({ to: "/progress/summarized" })
+                                                }}
+                                            >
+                                                요약
+                                            </Button>
+                                        </Vstack>
+                                    </RoundBox>
+                                </Labeled>
+
+                                <Labeled>
+                                    <Labeled.Header>오답 과제</Labeled.Header>
+                                    <RoundBox isBordered padding="md">
+                                        <Vstack gap="xs">
+                                            <Button
+                                                isOnLeft
+                                                onClick={() => {
+                                                    setIsSidebarOn(false)
+                                                    navigate({ to: "/review-check" })
+                                                }}
+                                            >
+                                                오답 과제 목록
+                                            </Button>
+                                            <Button
+                                                isOnLeft
+                                                onClick={() => {
+                                                    setIsSidebarOn(false)
+                                                    navigate({ to: "/review-check/book" })
+                                                }}
+                                            >
+                                                오답 체크
+                                            </Button>
+                                        </Vstack>
+                                    </RoundBox>
+                                </Labeled>
+                            </Vstack>
                         </RoundBox>
                     </motion.div>
                 </motion.div>
