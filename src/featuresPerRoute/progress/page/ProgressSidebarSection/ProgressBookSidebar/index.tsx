75822d70e8bbeecc1dc9ac9b72e56f899380c79d
Tynahan <nusilite@gmail.com>
2026-01-21 18:23:58 +0900

    feat(progress): add title for book sidebar

--- src/featuresPerRoute/progress/page/ProgressSidebarSection/ProgressBookSidebar/index.tsx
+++ src/featuresPerRoute/progress/page/ProgressSidebarSection/ProgressBookSidebar/index.tsx
@@ -1,5 +1,22 @@
+import { Vstack } from "@/packages/components/layouts"
+import Title from "@/packages/components/Title/Title"
+import { getRouteApi, useLoaderData } from "@tanstack/react-router"
+
+const route = getRouteApi("/progress/")
+
 const ProgressBookSidebar = () => {
-    return <div>this is progress book sidebar </div>
+    const { studentArray, classroomArray } = useLoaderData({ from: "/progress/" })
+    const { student: studentInParams, classroom: classroomInParams } = route.useSearch()
+
+    const classroom = classroomArray.find((el) => el.id === classroomInParams)
+    const student = studentArray.find((el) => el.id === studentInParams)
+    const title = [classroom?.name, student?.users.name].filter((value) => value).join(" / ")
+
+    return (
+        <Vstack>
+            <Title as="h3">{title}</Title>
+        </Vstack>
+    )
 }
 
 export default ProgressBookSidebar
