78447a6b2ff7242a77eefdeeee57983a85134faf
Tynahan <nusilite@gmail.com>
2026-01-21 14:45:19 +0900

    feat(progress): group studnets by classroom

--- src/featuresPerRoute/progress/page/ProgressSidebarSection/ProgressStudentSidebar/index.tsx
+++ src/featuresPerRoute/progress/page/ProgressSidebarSection/ProgressStudentSidebar/index.tsx
@@ -1,13 +1,58 @@
+import type { ExtendedStudent } from "@/featuresPerRoute/manage.student/types"
 import { Vstack } from "@/packages/components/layouts"
+import { ClientError } from "@/shared/error/clientError"
+import type { Classroom, ClassroomStudent } from "@/shared/interfaces"
 import { useLoaderData } from "@tanstack/react-router"
 
+type GroupByClassroomProps = {
+    studentArray: ExtendedStudent[]
+    classroomArray: Classroom[]
+    classroomStudentArray: ClassroomStudent[]
+}
+type ClassroomWithStudent = {
+    classroomName: string
+    studentArray: ExtendedStudent[]
+}
+type GroupByClassroomReturns = {
+    isolatedStudentArray: ExtendedStudent[]
+    classroomWithStudentArray: ClassroomWithStudent[]
+}
+const groupByClassroom = ({
+    studentArray,
+    classroomArray,
+    classroomStudentArray,
+}: GroupByClassroomProps): GroupByClassroomReturns => {
+    const isolatedStudentArray: ExtendedStudent[] = studentArray.filter((student) => {
+        const index = classroomStudentArray.findIndex((classroomStudent) => classroomStudent.student_id === student.id)
+        return index === -1
+    })
+
+    const classroomWithStudentArray: ClassroomWithStudent[] = classroomArray.map((classroom) => {
+        const filteredStudentArray = classroomStudentArray
+            .filter((classroomStudent) => classroomStudent.classroom_id === classroom.id)
+            .map((classroomStudent) => {
+                const student = studentArray.find((el) => el.id === classroomStudent.student_id)
+                if (!student) throw ClientError.Unexpected("학생을 못 찾았어요")
+                return student
+            })
+        const classroomWithStudent = {
+            classroomName: classroom.name,
+            studentArray: filteredStudentArray,
+        }
+        return classroomWithStudent
+    })
+
+    return { isolatedStudentArray, classroomWithStudentArray }
+}
+
 const ProgressStudentSidebar = () => {
-    const { studentArray, classroomArray, classroomStudentArray } = useLoaderData({ from: "/progress/" })
+    const loaderData = useLoaderData({ from: "/progress/" })
+    const { classroomWithStudentArray, isolatedStudentArray } = groupByClassroom(loaderData)
+
     return (
         <Vstack>
-            <p>{JSON.stringify(studentArray)}</p>
-            <p>{JSON.stringify(classroomArray)}</p>
-            <p>{JSON.stringify(classroomStudentArray)}</p>
+            <p>{JSON.stringify(classroomWithStudentArray)}</p>
+            <p>{JSON.stringify(isolatedStudentArray)}</p>
         </Vstack>
     )
 }
