bb13de5e8fd951a3fe4de9bdf785ac27b2928832
Tynahan <nusilite@gmail.com>
2026-01-21 14:15:56 +0900

    feat(progress): setup loader

--- src/featuresPerRoute/progress/loader/index.tsx
+++ src/featuresPerRoute/progress/loader/index.tsx
@@ -3,7 +3,7 @@ import type { QueryClient } from "@tanstack/react-query"
 
 const progressLoaderFn = (queryClient: QueryClient) => {
     // NOTE: 반, 학생 받아오는 것만 필요하니 기존 로더 재탕함
-    manageStudentLoaderFn({ queryClient })
+    return manageStudentLoaderFn({ queryClient })
 }
 
 export default progressLoaderFn
--- src/featuresPerRoute/progress/page/ProgressSidebarSection/ProgressStudentSidebar/index.tsx
+++ src/featuresPerRoute/progress/page/ProgressSidebarSection/ProgressStudentSidebar/index.tsx
@@ -1,5 +1,15 @@
+import { Vstack } from "@/packages/components/layouts"
+import { useLoaderData } from "@tanstack/react-router"
+
 const ProgressStudentSidebar = () => {
-    return <div>this is progress student sidebar</div>
+    const { studentArray, classroomArray, classroomStudentArray } = useLoaderData({ from: "/progress/" })
+    return (
+        <Vstack>
+            <p>{JSON.stringify(studentArray)}</p>
+            <p>{JSON.stringify(classroomArray)}</p>
+            <p>{JSON.stringify(classroomStudentArray)}</p>
+        </Vstack>
+    )
 }
 
 export default ProgressStudentSidebar
