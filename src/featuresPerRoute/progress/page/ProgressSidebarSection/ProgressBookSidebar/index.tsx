33f756f3d2f61dbe4e983e2c7f83a9aaa1a52f8c
Tynahan <nusilite@gmail.com>
2026-01-22 22:42:40 +0900

    feat(progress): conditionally render delete button

--- src/featuresPerRoute/progress/page/ProgressSidebarSection/ProgressBookSidebar/index.tsx
+++ src/featuresPerRoute/progress/page/ProgressSidebarSection/ProgressBookSidebar/index.tsx
@@ -37,21 +37,34 @@ const ProgressBookButton = ({ joinedBook }: ProgressBookButtonProps) => {
     const navigate = useNavigate({ from: "/progress/" })
     const searchParams = route.useSearch()
 
+    const deleteMutation = useMutation({
+        mutationFn: () =>
+            instance.delete(`/progress/book/${joinedBook?.book.id}`, {
+                params: { student_id: joinedBook?.student_id, classroom_id: joinedBook?.classroom_id },
+            }),
+    })
+
     const handleBodyClick = () => {
         navigate({ search: { ...searchParams, book_id: joinedBook?.book.id } })
     }
     const handleDeleteClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         event.stopPropagation()
+        deleteMutation.mutate()
     }
+
+    const isDeleteButtonVisible =
+        Boolean(searchParams.classroom_id) !== Boolean(searchParams.student_id) && joinedBook?.book.id
 
     return (
         <Button isBorderedOnHover color="black" isOnLeft onClick={handleBodyClick} className="grow">
             <Hstack className="w-full">
                 <p className="grow">{joinedBook ? joinedBook.book.title : "전체"}</p>
 
-                <Button color="black" isBorderedOnHover onClick={handleDeleteClick}>
-                    <X size={16} />
-                </Button>
+                {isDeleteButtonVisible && (
+                    <Button color="black" isBorderedOnHover onClick={handleDeleteClick}>
+                        <X size={16} />
+                    </Button>
+                )}
             </Hstack>
         </Button>
     )
