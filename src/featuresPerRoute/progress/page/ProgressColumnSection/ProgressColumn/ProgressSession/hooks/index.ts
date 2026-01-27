0941f2974b8c7a7aaa6aaa5578637fcc655436a6
Tynahan <nusilite@gmail.com>
2026-01-27 18:07:43 +0900

    refactor(progress): refactor session to split concerns

---
+++ src/featuresPerRoute/progress/page/ProgressColumnSection/ProgressColumn/ProgressSession/hooks/index.ts
@@ -0,0 +1,121 @@
+import type { ConciseSyllabus } from "@/featuresPerRoute/progress/types"
+import { instance } from "@/packages/api/axiosInstances"
+import { ClientError } from "@/shared/error/clientError"
+import useSimpleMutation from "@/shared/hooks/useSimpleMutation"
+import type { SessionStatus } from "@/shared/interfaces"
+import { getRouteApi } from "@tanstack/react-router"
+import type { ProgressSessionProps } from ".."
+
+const route = getRouteApi("/progress/")
+
+type ProgressSessionAdditionalData = {
+    syllabus_id: string
+    startingTopicTitle: string
+    session_id: string
+    status: SessionStatus | null
+}
+type ProgressSessionUpdateProps = { previous: ConciseSyllabus[]; additionalData: ProgressSessionAdditionalData }
+const useSessionMutation = (session_id?: string) => {
+    const searchParams = route.useSearch()
+
+    return useSimpleMutation({
+        queryKeyWithoutParams: ["progressSession"],
+        url: `/progress/session/assigned${session_id ? ["/", session_id].join("") : ""}`,
+        method: session_id ? "delete" : "post",
+        params: searchParams,
+        update: ({ previous, additionalData }: ProgressSessionUpdateProps) => {
+            const { session_id, status, syllabus_id, startingTopicTitle } = additionalData
+            const newData = [...previous]
+            const syllabus = newData.find((elSyllabus) => elSyllabus.id === syllabus_id)
+            const sessionsByTopic = syllabus?.sessionsByTopicArray.find(
+                (elSessionsByTopic) => elSessionsByTopic.title === startingTopicTitle
+            )
+            const session = sessionsByTopic?.conciseSessionArray.find(
+                (elConciseSession) => elConciseSession.id === session_id
+            )
+            if (!session) throw ClientError.Unexpected("묶음을 찾지 못했어요")
+            session.status = status
+            return newData
+        },
+    })
+}
+
+type UseEventHandlersProps = ProgressSessionProps & {
+    mutatePost: ReturnType<typeof useSessionMutation>["mutate"]
+    mutateDelete: ReturnType<typeof useSessionMutation>["mutate"]
+}
+const useEventHandlers = ({
+    conciseSession,
+    syllabus_id,
+    startingTopicTitle,
+    mutatePost,
+    mutateDelete,
+}: UseEventHandlersProps) => {
+    const searchParams = route.useSearch()
+    const { classroom_id, student_id } = searchParams
+    // TODO: dropdown에서 같은 걸 클랙해도 여전히 클릭이 되도록 수정해야
+    // TODO: 현재 상태를 제외하고 메뉴가 뜨도록 수정해야
+    const handleDropdownMenuChange = async (value: string) => {
+        const baseBody = {
+            session_id: conciseSession.id,
+            classroom_id,
+            student_id,
+        }
+
+        switch (value) {
+            case "homework": {
+                mutatePost({
+                    body: { ...baseBody, session_status: "HOMEWORK" },
+                    additionalData: {
+                        status: "HOMEWORK",
+                        session_id: conciseSession.id,
+                        startingTopicTitle,
+                        syllabus_id,
+                    },
+                })
+                break
+            }
+            case "today": {
+                mutatePost({
+                    body: { ...baseBody, session_status: "TODAY" },
+                    additionalData: {
+                        status: "TODAY",
+                        session_id: conciseSession.id,
+                        startingTopicTitle,
+                        syllabus_id,
+                    },
+                })
+                break
+            }
+            case "dismiss":
+                mutateDelete({
+                    body: undefined,
+                    additionalData: {
+                        status: null,
+                        session_id: conciseSession.id,
+                        startingTopicTitle,
+                        syllabus_id,
+                    },
+                })
+                break
+        }
+    }
+    const handleClickToComplete = async () => {
+        if (!conciseSession.status) return
+        await instance.post(`/progress/session/completed/${conciseSession.id}`, undefined, {
+            params: searchParams,
+        })
+    }
+
+    return { handleDropdownMenuChange, handleClickToComplete }
+}
+
+const useProgressSession = (props: ProgressSessionProps) => {
+    const { conciseSession } = props
+    const { mutate: mutatePost } = useSessionMutation()
+    const { mutate: mutateDelete } = useSessionMutation(conciseSession.id)
+
+    const eventHanderReturns = useEventHandlers({ ...props, mutatePost, mutateDelete })
+    return { ...eventHanderReturns }
+}
+export default useProgressSession
--- src/featuresPerRoute/progress/page/ProgressColumnSection/ProgressColumn/ProgressSession/index.tsx
+++ src/featuresPerRoute/progress/page/ProgressColumnSection/ProgressColumn/ProgressSession/index.tsx
@@ -1,15 +1,12 @@
-import type { ConciseSession, ConciseSyllabus } from "@/featuresPerRoute/progress/types"
-import { instance } from "@/packages/api/axiosInstances"
+import type { ConciseSession } from "@/featuresPerRoute/progress/types"
 import Button from "@/packages/components/Button/Button"
 import Dropdown from "@/packages/components/Dropdown/Dropdown"
 import { Hstack, Vstack } from "@/packages/components/layouts"
 import RoundBox from "@/packages/components/RoundBox"
-import { ClientError } from "@/shared/error/clientError"
-import useSimpleMutation from "@/shared/hooks/useSimpleMutation"
-import type { SessionStatus } from "@/shared/interfaces"
 import { getRouteApi } from "@tanstack/react-router"
 import clsx from "clsx"
 import { Ellipsis } from "lucide-react"
+import useProgressSession from "./hooks"
 
 type ProgressSessionLabelProps = {
     conciseSession: ConciseSession
@@ -38,103 +35,42 @@ const ProgressSessionLabel = ({ conciseSession }: ProgressSessionLabelProps) => 
     )
 }
 
-type ProgressSessionAdditionalData = {
-    syllabus_id: string
-    startingTopicTitle: string
-    session_id: string
-    status: SessionStatus | null
-}
-type ProgressSessionUpdateProps = { previous: ConciseSyllabus[]; additionalData: ProgressSessionAdditionalData }
-
 const route = getRouteApi("/progress/")
-type ProgressSessionProps = {
+
+type ProgressSessionDropdownProps = {
+    handleDropdownMenuChange: (value: string) => void
+}
+const ProgressSessionDropdown = ({ handleDropdownMenuChange }: ProgressSessionDropdownProps) => {
+    const searchParams = route.useSearch()
+    const { classroom_id, student_id } = searchParams
+    const isInteractable = Boolean(classroom_id) !== Boolean(student_id)
+    if (!isInteractable) return null
+    return (
+        <Dropdown>
+            <Dropdown.Trigger>
+                <Button color="black" isBorderedOnHover>
+                    <Ellipsis size={16} />
+                </Button>
+            </Dropdown.Trigger>
+            <Dropdown.Menu onChange={handleDropdownMenuChange}>
+                <Dropdown.MenuItem value="homework">숙제</Dropdown.MenuItem>
+                <Dropdown.MenuItem value="today">오늘</Dropdown.MenuItem>
+                <Dropdown.MenuItem value="dismiss">해제</Dropdown.MenuItem>
+            </Dropdown.Menu>
+        </Dropdown>
+    )
+}
+
+export type ProgressSessionProps = {
     conciseSession: ConciseSession
     syllabus_id: string
     startingTopicTitle: string
 }
-const ProgressSession = ({ conciseSession, syllabus_id, startingTopicTitle }: ProgressSessionProps) => {
-    const searchParams = route.useSearch()
-    const { classroom_id, student_id } = searchParams
+const ProgressSession = (props: ProgressSessionProps) => {
+    const { conciseSession } = props
 
-    const useSessionMutation = (session_id?: string) =>
-        useSimpleMutation({
-            queryKeyWithoutParams: ["progressSession"],
-            url: `/progress/session/assigned${session_id ? ["/", session_id].join("") : ""}`,
-            method: session_id ? "delete" : "post",
-            params: searchParams,
-            update: ({ previous, additionalData }: ProgressSessionUpdateProps) => {
-                const { session_id, status, syllabus_id, startingTopicTitle } = additionalData
-                const newData = [...previous]
-                const syllabus = newData.find((elSyllabus) => elSyllabus.id === syllabus_id)
-                const sessionsByTopic = syllabus?.sessionsByTopicArray.find(
-                    (elSessionsByTopic) => elSessionsByTopic.title === startingTopicTitle
-                )
-                const session = sessionsByTopic?.conciseSessionArray.find(
-                    (elConciseSession) => elConciseSession.id === session_id
-                )
-                if (!session) throw ClientError.Unexpected("묶음을 찾지 못했어요")
-                session.status = status
-                return newData
-            },
-        })
-    const postMutation = useSessionMutation()
-    const deleteMutation = useSessionMutation(conciseSession.id)
+    const { handleClickToComplete, handleDropdownMenuChange } = useProgressSession(props)
 
-    // TODO: dropdown에서 같은 걸 클랙해도 여전히 클릭이 되도록 수정해야
-    // TODO: 현재 상태를 제외하고 메뉴가 뜨도록 수정해야
-    const handleDropdownMenuChange = async (value: string) => {
-        const baseBody = {
-            session_id: conciseSession.id,
-            classroom_id,
-            student_id,
-        }
-
-        switch (value) {
-            case "homework": {
-                postMutation.mutate({
-                    body: { ...baseBody, session_status: "HOMEWORK" },
-                    additionalData: {
-                        status: "HOMEWORK",
-                        session_id: conciseSession.id,
-                        startingTopicTitle,
-                        syllabus_id,
-                    },
-                })
-                break
-            }
-            case "today": {
-                postMutation.mutate({
-                    body: { ...baseBody, session_status: "TODAY" },
-                    additionalData: {
-                        status: "TODAY",
-                        session_id: conciseSession.id,
-                        startingTopicTitle,
-                        syllabus_id,
-                    },
-                })
-                break
-            }
-            case "dismiss":
-                deleteMutation.mutate({
-                    body: undefined,
-                    additionalData: {
-                        status: null,
-                        session_id: conciseSession.id,
-                        startingTopicTitle,
-                        syllabus_id,
-                    },
-                })
-                break
-        }
-    }
-    const handleClickToComplete = async () => {
-        if (!conciseSession.status) return
-        await instance.post(`/progress/session/completed/${conciseSession.id}`, undefined, {
-            params: searchParams,
-        })
-    }
-
-    const isInteractable = Boolean(classroom_id) !== Boolean(student_id)
     return (
         <RoundBox
             onClick={handleClickToComplete}
@@ -151,20 +87,7 @@ const ProgressSession = ({ conciseSession, syllabus_id, startingTopicTitle }: Pr
         >
             <Hstack className="items-start" gap="none">
                 <ProgressSessionLabel conciseSession={conciseSession} />
-                {isInteractable && (
-                    <Dropdown>
-                        <Dropdown.Trigger>
-                            <Button color="black" isBorderedOnHover>
-                                <Ellipsis size={16} />
-                            </Button>
-                        </Dropdown.Trigger>
-                        <Dropdown.Menu onChange={handleDropdownMenuChange}>
-                            <Dropdown.MenuItem value="homework">숙제</Dropdown.MenuItem>
-                            <Dropdown.MenuItem value="today">오늘</Dropdown.MenuItem>
-                            <Dropdown.MenuItem value="dismiss">해제</Dropdown.MenuItem>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                )}
+                <ProgressSessionDropdown handleDropdownMenuChange={handleDropdownMenuChange} />
             </Hstack>
         </RoundBox>
     )
