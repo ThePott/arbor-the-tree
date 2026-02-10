c4b9b26f60e6a69429235eb2f8e6091c8e0ea3d1
Tynahan <nusilite@gmail.com>
2026-02-10 18:08:27 +0900

    feat(review): virtualize flat items in scroll

--- src/features/_sidebar._review.review.check/page/index.tsx
+++ src/features/_sidebar._review.review.check/page/index.tsx
@@ -1,5 +1,7 @@
 import { Container, FlexOneContainer, Vstack } from "@/packages/components/layouts"
 import RoundBox from "@/packages/components/RoundBox"
+import { useVirtualizer } from "@tanstack/react-virtual"
+import { useRef } from "react"
 import useReviewCheck from "./hooks"
 import ReviewCheckCreateToolbar from "./ReviewCheckCreateToolbar"
 import ReviewCheckFlatItemComponent from "./ReviewCheckFlatItemComponent"
@@ -8,6 +10,26 @@ import { makeReviewCheckFlatItemArray } from "./utils/make-review-check-flat-ite
 const ReviewCheckPage = () => {
     const { data } = useReviewCheck()
     const flatItemArray = makeReviewCheckFlatItemArray(data)
+
+    const parentRef = useRef<HTMLDivElement>(null)
+
+    // eslint-disable-next-line react-hooks/incompatible-library
+    const rowVirtualizer = useVirtualizer({
+        count: flatItemArray.length,
+        getScrollElement: () => parentRef.current,
+        estimateSize: (index) => {
+            const forWhat = flatItemArray[index].forWhat
+            switch (forWhat) {
+                case "topicHeader":
+                    return 24
+                case "stepHeader":
+                    return 24
+                case "pagenatedQuestions":
+                    return 48
+            }
+        },
+        overscan: 5,
+    })
 
     if (flatItemArray.length === 0) {
         return (
@@ -20,10 +42,29 @@ const ReviewCheckPage = () => {
     return (
         <Vstack gap="none" className="h-full overflow-hidden">
             <ReviewCheckCreateToolbar />
-            <FlexOneContainer isYScrollable>
-                <Container isPadded width="md">
-                    {flatItemArray.map((flatItem) => (
-                        <ReviewCheckFlatItemComponent flatItem={flatItem} />
+            <FlexOneContainer isYScrollable ref={parentRef}>
+                <Container
+                    isPadded
+                    width="md"
+                    style={{
+                        height: `${rowVirtualizer.getTotalSize()}px`,
+                        position: "relative",
+                    }}
+                >
+                    {rowVirtualizer.getVirtualItems().map((virtualItem) => (
+                        <div
+                            key={virtualItem.key}
+                            style={{
+                                position: "absolute",
+                                top: 0,
+                                left: 0,
+                                width: "100%",
+                                height: `${virtualItem.size}px`,
+                                transform: `translateY(${virtualItem.start}px)`,
+                            }}
+                        >
+                            <ReviewCheckFlatItemComponent flatItem={flatItemArray[virtualItem.index]} />
+                        </div>
                     ))}
                 </Container>
             </FlexOneContainer>
