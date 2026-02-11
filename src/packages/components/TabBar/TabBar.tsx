7dae080d699adabb099f9a664839fdd78d54297d
Tynahan <nusilite@gmail.com>
2026-02-11 11:45:20 +0900

    feat(common): add background props in tabbar to change colors

--- src/packages/components/TabBar/TabBar.tsx
+++ src/packages/components/TabBar/TabBar.tsx
@@ -1,51 +1,53 @@
 import { makeTransition } from "@/shared/utils/animation"
+import clsx from "clsx"
 import { motion } from "motion/react"
 import { useState } from "react"
 import { Hstack } from "../layouts"
 
-export interface Tab<T extends string> {
-    label: string
-    value: T
+type BackgroundProps = {
+    additionalClassName?: string
 }
-
-type TabVariant = "underline" | "pill"
-
-const TabBackgroundPill = () => {
+const TabBackgroundPill = ({ additionalClassName }: BackgroundProps) => {
     return (
         <motion.div
             layout
             layoutId="selected-pill"
             style={{ borderRadius: 9999 }}
-            className="bg-bg-3 absolute inset-0"
+            className={clsx("bg-bg-3 absolute inset-0", additionalClassName)}
             transition={makeTransition()}
         />
     )
 }
-
-const TabBackgroundUnderline = () => {
+const TabBackgroundUnderline = ({ additionalClassName }: BackgroundProps) => {
     return (
         <motion.div
             layout
             layoutId="tab-background"
-            className="bg-fg-vivid absolute right-0 bottom-0 left-0 h-0.5"
+            className={clsx("bg-fg-vivid absolute right-0 bottom-0 left-0 h-0.5", additionalClassName)}
             transition={makeTransition()}
         />
     )
 }
 
-interface TabItemProps<T extends string> {
+type TabVariant = "underline" | "pill"
+export type Tab<T extends string> = {
+    label: string
+    value: T
+}
+type TabItemProps<T extends string> = {
     variant: TabVariant
     tab: Tab<T>
     isSelected: boolean
     onClick: () => void
+    backgroundProps?: BackgroundProps
 }
-const TabItem = <T extends string>({ variant, tab, isSelected, onClick }: TabItemProps<T>) => {
+const TabItem = <T extends string>({ variant, tab, isSelected, onClick, backgroundProps }: TabItemProps<T>) => {
     return (
         <div className="relative cursor-pointer px-3 py-2" onClick={onClick}>
             {isSelected && (
                 <>
-                    {variant === "underline" && <TabBackgroundUnderline />}
-                    {variant === "pill" && <TabBackgroundPill />}
+                    {variant === "underline" && <TabBackgroundUnderline {...backgroundProps} />}
+                    {variant === "pill" && <TabBackgroundPill {...backgroundProps} />}
                 </>
             )}
             <p className="relative z-10">{tab.label}</p>
@@ -53,12 +55,13 @@ const TabItem = <T extends string>({ variant, tab, isSelected, onClick }: TabIte
     )
 }
 
-interface TabBar<T extends string> {
+type TabBar<T extends string> = {
     variant: TabVariant
     tabArray: Tab<T>[]
     onSelect: (tab: Tab<T>) => void
+    backgroundProps?: BackgroundProps
 }
-const TabBar = <T extends string>({ variant, tabArray, onSelect }: TabBar<T>) => {
+const TabBar = <T extends string>({ variant, tabArray, onSelect, backgroundProps }: TabBar<T>) => {
     const [selectedTabValue, setSelectedTabValue] = useState(tabArray[0].value)
     const handleTabClick = (tab: Tab<T>) => {
         setSelectedTabValue(tab.value)
@@ -68,6 +71,7 @@ const TabBar = <T extends string>({ variant, tabArray, onSelect }: TabBar<T>) =>
         <Hstack>
             {tabArray.map((tab) => (
                 <TabItem
+                    backgroundProps={backgroundProps}
                     key={tab.value}
                     variant={variant}
                     tab={tab}
