f775b048cdb654dd2890bf2c8e6fbc932b6ee770
Tynahan <nusilite@gmail.com>
2026-02-11 12:34:18 +0900

    chore(lint): fix lint error

--- src/features/manage.student/types/index.ts
+++ src/features/manage.student/types/index.ts
@@ -4,7 +4,7 @@ import z from "zod/v3"
 export type ExtendedStudent = Student & { school: School } & { users: AppUser }
 
 export const manageStudentSearchArray = ["classroom", "student"] as const
-export type tanageStudentSearch = (typeof manageStudentSearchArray)[number]
+export type ManageStudentSearch = (typeof manageStudentSearchArray)[number]
 export const manageStudentSearchSchema = z.object({
     by: z.enum(manageStudentSearchArray).optional(),
 })
