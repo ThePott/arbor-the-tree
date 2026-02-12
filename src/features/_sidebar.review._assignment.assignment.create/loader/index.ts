3b7f2a25ae3be96eeb6e2b7eecd6ae0f3480dad1
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-02-12 16:34:04 +0900

    feat(assignment): setup loader for create

---
+++ src/features/_sidebar.review._assignment.assignment.create/loader/index.ts
@@ -0,0 +1,26 @@
+import { instance } from "@/packages/api/axiosInstances"
+import type { QueryClient } from "@tanstack/react-query"
+
+type ReviewAssignmentCreateLoaderFnProps = {
+    queryClient: QueryClient
+    classroom_id: string | undefined
+    student_id: string | undefined
+}
+const reviewAssignmentCreateLoaderFn = async ({
+    queryClient,
+    classroom_id,
+    student_id,
+}: ReviewAssignmentCreateLoaderFnProps) => {
+    if (!student_id) return null
+
+    const data = queryClient.ensureQueryData({
+        queryKey: ["reviewAssignmentCreate", classroom_id, student_id],
+        queryFn: async () => {
+            const response = await instance.get("/review/assignment/create", { params: { classroom_id, student_id } })
+            return response.data
+        },
+    })
+    return data
+}
+
+export default reviewAssignmentCreateLoaderFn
