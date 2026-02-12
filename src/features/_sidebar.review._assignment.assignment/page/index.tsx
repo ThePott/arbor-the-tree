7b9902e49fc9177ef1358b0dea19933bea6a2578
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-02-12 13:41:17 +0900

    feat(assignment): setup assignment layout

--- src/features/_sidebar.review._assignment.assignment/page/index.tsx
+++ src/features/_sidebar.review._assignment.assignment/page/index.tsx
@@ -1,16 +1,24 @@
-import Button from "@/packages/components/Button/Button"
+import { Container, Vstack } from "@/packages/components/layouts"
 import RoundBox from "@/packages/components/RoundBox"
-import { useNavigate } from "@tanstack/react-router"
+import TabBar, { type Tab } from "@/packages/components/TabBar/TabBar"
+import Title from "@/packages/components/Title/Title"
+
+type TabStatus = "uncompleted" | "all"
+const tabArray: Tab<TabStatus>[] = [
+    { label: "미완", value: "uncompleted" },
+    { label: "전체", value: "all" },
+]
 
 const ReviewAssignmentPage = () => {
-    const navigate = useNavigate()
     return (
-        <RoundBox color="bg0" radius="lg" isShadowed padding="xl">
-            <Button color="bg2" onClick={() => navigate({ to: "/review/check" })}>
-                review check
-            </Button>
-            <p>not yet created </p>
-        </RoundBox>
+        <Container isPadded>
+            <RoundBox color="bg0" radius="lg" isShadowed padding="xl">
+                <Vstack gap="lg">
+                    <Title as="h1">오답 과제 목록</Title>
+                    <TabBar onSelect={() => {}} tabArray={tabArray} variant="underline" />
+                </Vstack>
+            </RoundBox>
+        </Container>
     )
 }
 
