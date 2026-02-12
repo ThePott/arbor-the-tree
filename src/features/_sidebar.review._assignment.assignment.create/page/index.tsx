e7b94424c0392ab315773af69fe1c00bd3adcb02
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-02-12 16:43:59 +0900

    feat(assignment): add mutation placeholder

--- src/features/_sidebar.review._assignment.assignment.create/page/index.tsx
+++ src/features/_sidebar.review._assignment.assignment.create/page/index.tsx
@@ -2,6 +2,7 @@ import Button from "@/packages/components/Button/Button"
 import { Container, Hstack, Vstack } from "@/packages/components/layouts"
 import RoundBox from "@/packages/components/RoundBox"
 import Title from "@/packages/components/Title/Title"
+import useSimpleMutation from "@/shared/hooks/useSimpleMutation"
 import { useQuery } from "@tanstack/react-query"
 import { getRouteApi } from "@tanstack/react-router"
 import { makeReviewAssignmentCreateQueryOptions } from "../loader"
@@ -10,6 +11,12 @@ const route = getRouteApi("/_sidebar")
 const ReviewAssignmentCreatePage = () => {
     const { classroom_id, student_id } = route.useSearch()
     const { data } = useQuery(makeReviewAssignmentCreateQueryOptions({ classroom_id, student_id }))
+    const { mutate } = useSimpleMutation({
+        method: "post",
+        url: "/review/assignment/create",
+        queryKey: ["reviewAssignmentCreate", classroom_id, student_id],
+        update: () => {},
+    })
     return (
         <Container isPadded>
             <RoundBox padding="xl" radius="lg" isShadowed color="bg0">
@@ -22,7 +29,11 @@ const ReviewAssignmentCreatePage = () => {
                         <Button padding="wide" border="always" color="transparent">
                             미리 보기
                         </Button>
-                        <Button padding="wide" color="green">
+                        <Button
+                            padding="wide"
+                            color="green"
+                            onClick={() => mutate({ body: undefined, additionalData: undefined })}
+                        >
                             제작
                         </Button>
                     </Hstack>
