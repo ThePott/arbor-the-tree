2efa292a3a66b49474c1c1b4c6db9a94c4fe5354
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-02-12 15:53:27 +0900

    feat(assignment): setup layout for assignment create page

--- src/features/_sidebar.review._assignment.assignment.create/page/index.tsx
+++ src/features/_sidebar.review._assignment.assignment.create/page/index.tsx
@@ -1,5 +1,27 @@
+import Button from "@/packages/components/Button/Button"
+import { Container, Hstack, Vstack } from "@/packages/components/layouts"
+import RoundBox from "@/packages/components/RoundBox"
+import Title from "@/packages/components/Title/Title"
+
 const ReviewAssignmentCreatePage = () => {
-    return <div>this is review assignment crate page placeholder</div>
+    return (
+        <Container isPadded>
+            <RoundBox padding="xl" radius="lg" isShadowed color="bg0">
+                <Vstack>
+                    <Title as="h1">오답 과제 제작</Title>
+
+                    <Hstack>
+                        <Button padding="wide" border="always" color="transparent">
+                            미리 보기
+                        </Button>
+                        <Button padding="wide" color="green">
+                            제작
+                        </Button>
+                    </Hstack>
+                </Vstack>
+            </RoundBox>
+        </Container>
+    )
 }
 
 export default ReviewAssignmentCreatePage
