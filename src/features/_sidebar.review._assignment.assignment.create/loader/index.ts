0741b9e7cfd3e52004cfa1c5542ea9f5da448e01
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-02-12 16:41:35 +0900

    feat(assignment): setup query for assignment create page

--- src/features/_sidebar.review._assignment.assignment.create/loader/index.ts
+++ src/features/_sidebar.review._assignment.assignment.create/loader/index.ts
@@ -1,6 +1,23 @@
 import { instance } from "@/packages/api/axiosInstances"
 import type { QueryClient } from "@tanstack/react-query"
 
+type MakeReviewAssignmentCreateQueryOptionsProps = {
+    classroom_id: string | undefined
+    student_id: string | undefined
+}
+export const makeReviewAssignmentCreateQueryOptions = ({
+    classroom_id,
+    student_id,
+}: MakeReviewAssignmentCreateQueryOptionsProps) => {
+    return {
+        queryKey: ["reviewAssignmentCreate", classroom_id, student_id],
+        queryFn: async () => {
+            const response = await instance.get("/review/assignment/create", { params: { classroom_id, student_id } })
+            return response.data
+        },
+    }
+}
+
 type ReviewAssignmentCreateLoaderFnProps = {
     queryClient: QueryClient
     classroom_id: string | undefined
@@ -13,13 +30,7 @@ const reviewAssignmentCreateLoaderFn = async ({
 }: ReviewAssignmentCreateLoaderFnProps) => {
     if (!student_id) return null
 
-    const data = queryClient.ensureQueryData({
-        queryKey: ["reviewAssignmentCreate", classroom_id, student_id],
-        queryFn: async () => {
-            const response = await instance.get("/review/assignment/create", { params: { classroom_id, student_id } })
-            return response.data
-        },
-    })
+    const data = queryClient.ensureQueryData(makeReviewAssignmentCreateQueryOptions({ classroom_id, student_id }))
     return data
 }
 
--- src/features/_sidebar.review._assignment.assignment.create/page/index.tsx
+++ src/features/_sidebar.review._assignment.assignment.create/page/index.tsx
@@ -2,13 +2,21 @@ import Button from "@/packages/components/Button/Button"
 import { Container, Hstack, Vstack } from "@/packages/components/layouts"
 import RoundBox from "@/packages/components/RoundBox"
 import Title from "@/packages/components/Title/Title"
+import { useQuery } from "@tanstack/react-query"
+import { getRouteApi } from "@tanstack/react-router"
+import { makeReviewAssignmentCreateQueryOptions } from "../loader"
 
+const route = getRouteApi("/_sidebar")
 const ReviewAssignmentCreatePage = () => {
+    const { classroom_id, student_id } = route.useSearch()
+    const { data } = useQuery(makeReviewAssignmentCreateQueryOptions({ classroom_id, student_id }))
     return (
         <Container isPadded>
             <RoundBox padding="xl" radius="lg" isShadowed color="bg0">
                 <Vstack>
                     <Title as="h1">오답 과제 제작</Title>
+
+                    <p>{JSON.stringify(data)}</p>
 
                     <Hstack>
                         <Button padding="wide" border="always" color="transparent">
