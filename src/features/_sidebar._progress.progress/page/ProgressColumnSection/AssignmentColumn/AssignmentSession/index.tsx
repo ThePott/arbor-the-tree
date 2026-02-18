2bca8e2af0171d37dfb1b96361753d3bbdc99556
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-02-18 12:58:19 +0900

    feat(assignment): add dropdown placeholder

--- src/features/_sidebar._progress.progress/page/ProgressColumnSection/AssignmentColumn/AssignmentSession/index.tsx
+++ src/features/_sidebar._progress.progress/page/ProgressColumnSection/AssignmentColumn/AssignmentSession/index.tsx
@@ -1,6 +1,27 @@
 import type { ReviewAssignmentMetaInfo } from "@/features/_sidebar.review._assignment.assignment/type"
+import Button from "@/packages/components/Button/Button"
+import Dropdown from "@/packages/components/Dropdown"
 import { isSameDay, makeFromNow } from "@/shared/utils/dateManipulations"
+import { Ellipsis } from "lucide-react"
 import StatusCompletenessBox from "../../ColumnWithBoxes/StatusCompletenessBox"
+
+type AssignmentSessionDropdownProps = { assignmentMetaInfo: ReviewAssignmentMetaInfo }
+const AssignmentSessionDropdown = ({ assignmentMetaInfo }: AssignmentSessionDropdownProps) => {
+    return (
+        <Dropdown>
+            <Dropdown.Trigger>
+                <Button color="transparent" border="onHover" padding="tight">
+                    <Ellipsis size={16} />
+                </Button>
+            </Dropdown.Trigger>
+            <Dropdown.Menu>
+                <Dropdown.MenuItem onClick={() => {}}>숙제</Dropdown.MenuItem>
+                <Dropdown.MenuItem onClick={() => {}}>할당</Dropdown.MenuItem>
+                <Dropdown.MenuItem onClick={() => {}}>해제</Dropdown.MenuItem>
+            </Dropdown.Menu>
+        </Dropdown>
+    )
+}
 
 type AssignmentSessionProps = { assignmentMetaInfo: ReviewAssignmentMetaInfo }
 const AssignmentSession = ({ assignmentMetaInfo }: AssignmentSessionProps) => {
@@ -22,6 +43,7 @@ const AssignmentSession = ({ assignmentMetaInfo }: AssignmentSessionProps) => {
                     <StatusCompletenessBox.Label role="sub">{`${makeFromNow(completed_at)} 완료`}</StatusCompletenessBox.Label>
                 )}
             </StatusCompletenessBox.LabelGroup>
+            <AssignmentSessionDropdown assignmentMetaInfo={assignmentMetaInfo} />
         </StatusCompletenessBox>
     )
 }
