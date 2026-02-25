e98b5130f25af7f1d514d6305f26dbdb75bbfad7
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-02-25 16:45:17 +0900

    fix(check): fix findJoinedQuestion logic

--- src/features/_sidebar._assigned._check.check/utils/index.ts
+++ src/features/_sidebar._assigned._check.check/utils/index.ts
@@ -60,23 +60,24 @@ export const findJoinedQuestion = <T extends ReviewCheckResponseData>({
     orderInfo,
 }: FindJoinedQuestionProps<T>): JoinedQuestion => {
     const question_id = changedEntry?.[0]
-    const checkboxIndex = orderInfo?.checkboxIndex
     const titleIndex = changedEntry?.[1].indexInfo.titleIndex ?? orderInfo?.titleIndex
     const subtitleIndex = changedEntry?.[1].indexInfo.subtitleIndex ?? orderInfo?.subtitleIndex
+    const checkboxIndex = orderInfo?.checkboxIndex
 
     // NOTE: id로 찾을 수도 있고 order로 찾을 수도 있다 << orderInfo 넣느냐 안 넣느냐에 따라 달라짐 << 이건 유지해야 함
     // NOTE: order 없이 찾을 때 ---- mutate 할 때 addtional data 가지고 optimistic update. 이 땐 order info 넣지 않음
     // NOTE: order로 찾을 때 ---- detect recent to changed 할 때, 한 개만 다중 선택한 걸 changed 로 만들 땐 order info 만으로 찾아냄
-    if (!question_id && !checkboxIndex) throw ClientError.Unexpected("오답 체크를 실패했어요")
-    if (!titleIndex || !subtitleIndex) throw ClientError.Unexpected("오답 체크를 실패했어요")
+    // NOTE: index === 0 은 정상이지만 falsy함. guard 할 때 주의
+    if (!question_id && checkboxIndex === undefined) throw ClientError.Unexpected("오답 체크를 실패했어요")
+    if (titleIndex === undefined || subtitleIndex === undefined) throw ClientError.Unexpected("오답 체크를 실패했어요")
 
-    const targetTopic = queryData?.topics.find((_, index) => index === titleIndex)
+    const targetTopic = queryData?.topics[titleIndex]
     if (!targetTopic) throw ClientError.Unexpected("오답 체크를 실패했어요")
-    const targetStep = targetTopic.steps.find((elStep) => elStep.order === subtitleIndex)
+    const targetStep = targetTopic.steps[subtitleIndex]
     if (!targetStep) throw ClientError.Unexpected("오답 체크를 실패했어요")
-    const targetQuestion = targetStep.questions.find((elQuestion) => {
+    const targetQuestion = targetStep.questions.find((elQuestion, index) => {
         if (question_id) return elQuestion.id === question_id
-        return elQuestion.order === checkboxIndex
+        return index === checkboxIndex
     })
     if (!targetQuestion) throw ClientError.Unexpected("오답 체크를 실패했어요")
     return targetQuestion
