e11b1303493b109a8f6151a8f24f68518904c8aa
Tynahan <nusilite@gmail.com>
2026-03-09 18:51:56 +0900

    style(manage): fix scrollbar jiggling

--- src/features/root/RootLayout.tsx
+++ src/features/root/RootLayout.tsx
@@ -1,12 +1,9 @@
 import { FlexOneContainer, FullScreen, Vstack } from "@/packages/components/layouts"
-import useGlobalStore from "@/shared/store/globalStore"
 import { Outlet } from "@tanstack/react-router"
 import Header from "./Header"
 import Sidebar from "./Sidebar"
 
 const RootLayout = () => {
-    const isBodyScrollable = useGlobalStore((state) => state.isBodyScrollable)
-
     return (
         <FullScreen className="relative">
             <Sidebar />
