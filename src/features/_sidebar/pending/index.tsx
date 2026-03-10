0dcce0280d1f4e30a6857cfa70f6e527001a811c
Tynahan <nusilite@gmail.com>
2026-03-10 11:49:23 +0900

    feat(skeleton): add classroom sidebar skeleton

--- src/features/_sidebar/pending/index.tsx
+++ src/features/_sidebar/pending/index.tsx
@@ -1,5 +1,20 @@
+import { Hstack, Vstack } from "@/packages/components/layouts"
+import Skeleton from "@/packages/components/Skeleton"
+import { Outlet } from "@tanstack/react-router"
+
 const SidebarPending = () => {
-    return <div>need to add sidebar skeleton</div>
+    return (
+        <Hstack gap="none" className="w-full h-full overflow-hidden">
+            <Vstack className="overflow-y-auto p-my-lg pr-0 w-[200px]" gap="sm">
+                <Skeleton widthInPixel={120} heightInPixel={24} />
+                <Skeleton heightInPixel={52} />
+                <Skeleton heightInPixel={52} />
+                <Skeleton heightInPixel={52} />
+                <Skeleton heightInPixel={52} />
+            </Vstack>
+            <Outlet />
+        </Hstack>
+    )
 }
 
 export default SidebarPending
